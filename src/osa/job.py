cdf4a16dc197b9dba5e1b0962ee196dd1907e84b
Manuel19032002 <manuelmartinezherresanchez@gmail.com>
2026-08-07 10:56:01 +0200

    Update history file pattern to include '.0'

--- src/osa/job.py
+++ src/osa/job.py
@@ -102,7 +102,7 @@ def are_all_jobs_correctly_finished(sequence_list):
         if sequence.type != "DATA":
             continue
         else:
-            history_files_list = analysis_directory.rglob(f"*{sequence.run}*.history")
+            history_files_list = analysis_directory.rglob(f"*{sequence.run}.0*.history")
 
         if not options.test:
             try:
